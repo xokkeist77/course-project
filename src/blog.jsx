f412c9c1f2d0e35e6e029d8783440279358d2e94
Dmitry Kiryak <dkiryak@gmail.com>
2025-04-17 22:40:03 +0300

    Реализовать основной компонент блока с роутингом

---
+++ src/blog.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  padding: 120px 0;
+`;
+const H2 = styled.h2`
+  text-align: center;
+`;
+
+const Header = () => <div>Шапка</div>;
+const Footer = () => <div>Футер</div>;
+
+export const Blog = () => {
+  return (
+    <>
+      <Header />
+      <Content>
+        <H2>Контент страницы</H2>
+        <Routes>
+          <Route path="/" element={<div>Главная страница</div>} />
+          <Route path="/login" element={<div>Авторизация</div>} />
+          <Route path="/register" element={<div>Регистрация</div>} />
+          <Route path="/users" element={<div>Пользователи</div>} />
+          <Route path="/post/:post_id" element={<div>Статья</div>} />
+          <Route path="/post/:post_id" element={<div>Новая статья</div>} />
+          <Route path="*" element={<div>Ошибка</div>} />
+        </Routes>
+      </Content>
+      <Footer />
+    </>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Blog } from './blog.jsx';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Blog />
+  </BrowserRouter>,
 );
